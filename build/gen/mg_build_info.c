7cf1ecff865d0e059e526417da4b83faf4e191f3
David <david.fc@inventoteca.com>
2022-02-05 15:23:01 -0600

    gitignore

--- build/gen/mg_build_info.c
+++
@@ -1,4 +0,0 @@
-/* Auto-generated, do not edit. */
-const char *mg_build_id = "20220205-210012/2.19.1-gd52032f";
-const char *mg_build_timestamp = "2022-02-05T21:00:12Z";
-const char *mg_build_version = "2.19.1";
